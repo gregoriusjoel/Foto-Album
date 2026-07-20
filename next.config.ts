0838c382f65b926417ef931b9296341bd7999308
Gregorius Joel <gregoriusjoell@gmail.com>
2026-07-20 17:15:07 +0700

    fix: add multi-level AWS S3 hostname patterns to Next.js image remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -27,9 +27,11 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: origins,
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: '*.r2.cloudflarestorage.com' },
-      { protocol: 'https', hostname: '*.cloudflare.com' },
-      { protocol: 'https', hostname: '*.amazonaws.com' },
+      { protocol: 'https', hostname: '**.r2.cloudflarestorage.com' },
+      { protocol: 'https', hostname: '**.cloudflare.com' },
+      { protocol: 'https', hostname: '**.amazonaws.com' },
+      { protocol: 'https', hostname: '**.s3.amazonaws.com' },
+      { protocol: 'https', hostname: '**.s3.ap-southeast-1.amazonaws.com' },
       { protocol: 'http',  hostname: 'localhost' },
       { protocol: 'http',  hostname: '127.0.0.1' },
     ],
